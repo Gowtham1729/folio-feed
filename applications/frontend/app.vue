996e8c2a3f83d38dbb24e9217e2d115a2cf43b9f
gowtham1729 <gowtham@alpaca-tech.ai>
2023-11-04 00:06:56 +0900

    feat: add basic api use in nuxt

--- applications/frontend/app.vue
+++ applications/frontend/app.vue
@@ -1,6 +1,32 @@
 <template>
+  <button @click="getNews">
+    Get News
+  </button>
   <div>
-    <NuxtWelcome />
+    {{ news }}
   </div>
-  <div>Hello World Test 2</div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  data() {
+    return {
+      news: []
+    }
+  },
+  methods: {
+    getNews() {
+      axios.get('http://localhost:8000/api/news/')
+        .then((response) => {
+          this.news = response.data;
+          console.log(this.news);
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  }
+}
+</script>
