042c46f8e82b2aef76e2990277da7d6f68e60d2e
gowtham1729 <gowtham@alpaca-tech.ai>
2023-11-05 18:43:23 +0900

    feat: Add a basic user input template

--- applications/frontend/app.vue
+++ applications/frontend/app.vue
@@ -1,32 +1,50 @@
 <template>
-  <button @click="getNews">
-    Get News
-  </button>
   <div>
-    {{ news }}
+    <h1>Welcome to the Folio Feed web application</h1>
+    <h3>This nuxt js website is hosted on Google Cloud Platform</h3>
+
+    <div>
+      <input v-model="userInput" placeholder="Type something..."/>
+
+      <button @click="addInputToList">OK</button>
+    </div>
+
+    <p><b>Echo: </b></p>
+    <ul>
+      <li v-for="(input, index) in inputsList" :key="index">
+        {{ input }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 export default {
-  name: 'App',
   data() {
     return {
-      news: []
-    }
+      userInput: '',
+      inputsList: [],
+    };
   },
   methods: {
-    getNews() {
-      axios.get('http://localhost:8000/api/news/')
-        .then((response) => {
-          this.news = response.data;
-          console.log(this.news);
-        })
-        .catch((error) => {
-          console.error(error)
-        })
+    addInputToList() {
+      if (this.userInput.trim()) {
+        this.inputsList.push(this.userInput);
+        this.userInput = '';
+      }
     }
   }
-}
+};
 </script>
+
+
+<style>
+button {
+  margin-left: 8px;
+}
+
+
+input {
+  margin-right: 8px;
+}
+</style>
